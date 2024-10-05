7e4f85b60ccd312c703b45a1014a5e16a9dfda65
“Akshay <paraba777@gmail.com>
2024-10-06 01:58:08 +0530

    alert

---
+++ 1_Basics_I/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// alert("Hello World")
+
+let js = 'amazing';
+if (js ==='amazing'){
+    alert('JS is FUN!')
+}
+
+js = 'boring';
+if (js ==='amazing'){
+    alert('JS is FUN!')
+}
